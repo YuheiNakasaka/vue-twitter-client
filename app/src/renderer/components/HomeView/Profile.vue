b154fb376187efd2f363c2466d4dd027a221588b
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2017-03-30 15:13:17 +0900

    make profile modal ui

--- app/src/renderer/components/HomeView/Profile.vue
+++ app/src/renderer/components/HomeView/Profile.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="profile">
-    <!-- {{ tweet }} -->
+    {{ tweet }}
     <div class="overlay"></div>
     <div class="profile-main">
       <div class="box">
         <div class="main">
           <div class="inner">
+            <div class="overlay"></div>
+            <div class="bk-img" :style="bkImgStyle(tweet)"></div>
             <div class="icon">
               <span>
                 <img :src="tweet.user.profile_image_url"/>
@@ -21,20 +23,32 @@
               <span>{{ tweet.user.description }}</span>
             </div>
             <div class="meta">
+              <span class="location">{{ tweet.user.location }}</span>
+              <span class="url">
+                <a @click="openInOsBrowser(tweet.user.url)">{{ tweet.user.url }}</a>
+              </span>
             </div>
           </div>
         </div>
         <div class="bottom">
           <div class="inner">
-            <div class="tweets">
+            <div class="tweets column">
+              <span class="label">TWEETS</span>
+              <span class="value">{{ tweet.user.statuses_count }}</span>
             </div>
-            <div class="followings">
+            <div class="followings column">
+              <span class="label">FOLLOWING</span>
+              <span class="value">{{ tweet.user.friends_count }}</span>
             </div>
-            <div class="followers">
+            <div class="followers column">
+              <span class="label">FOLLOWERS</span>
+              <span class="value">{{ tweet.user.followers_count }}</span>
             </div>
-            <div class="listed">
+            <div class="listed column">
+              <span class="label">LISTED</span>
+              <span class="value">{{ tweet.user.listed_count }}</span>
             </div>
-            <div class="btns">
+            <div class="btns column">
             </div>
           </div>
         </div>
@@ -45,6 +59,7 @@
 
 <script>
   import { mapState } from 'vuex'
+  import { shell } from 'electron'
 
   export default {
     name: 'profile',
@@ -56,6 +71,21 @@
       ...mapState([
         'user'
       ])
+    },
+    methods: {
+      openInOsBrowser (url) {
+        shell.openExternal(url)
+      },
+      bkImgUrl (tweet) {
+        if (tweet.user.profile_banner_url) {
+          return tweet.user.profile_banner_url
+        } else if (tweet.user.profile_background_image_url) {
+          return tweet.user.profile_background_image_url
+        }
+      },
+      bkImgStyle (tweet) {
+        return 'background-image: url("' + this.bkImgUrl(tweet) + '"); background-color: #' + tweet.user.profile_background_color + ';background-size: cover; background-position: center; height: 230px; width: 100%;'
+      }
     }
   }
 </script>
@@ -79,7 +109,7 @@
   left: 50%;
   display: block;
   width: 450px;
-  height: 300px;
+  height: 280px;
   margin: -150px 0 0 -200px;
   border-radius: 4px;
   z-index: 2001;
@@ -87,38 +117,108 @@
   .box {
     .main {
       width: 100%;
-      .icon {
+      height: 230px;
+      padding: 0 10px;
+      position: relative;
+      z-index: 1;
+      span {
+        position: relative;
+        z-index: 2;
         width: 100%;
-        padding: 40px 0;
+        color: #fff;
+      }
+      .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #353535;
+        opacity: 0.2;
+        z-index: 1;
+      }
+      .bk-img {
+        position: absolute;
+        width: 100%;
+        height: 230px;
+        overflow: hidden;
+        top: 0;
+        left: 0;
+        z-index: 0;
+        text-align: center;
+        border-radius: 4px 4px 0 0;
+      }
+      .icon {
+        padding: 15px 0;
         text-align: center;
         img {
-          width: 44px;
-          height: 44px;
+          width: 55px;
+          height: 55px;
+          padding: 1px;
           border-radius: 4px;
+          background-color: #353535;
         }
       }
       .name {
-        width: 100%;
         margin-bottom: 10px;
         text-align: center;
-        font-size: 14px;
+        font-size: 16px;
         font-weight: bold;
       }
       .screen-name {
-        width: 100%;
         margin-bottom: 10px;
         text-align: center;
         font-size: 12px;
       }
       .description {
-        width: 100%;
+        line-height: 1.4;
+        margin-bottom: 10px;
         text-align: center;
-        font-size: 12px;
+        font-size: 11px;
         span {
           text-align: left;
         }
       }
       .meta {
+        text-align: center;
+        font-size: 12px;
+        .url {
+          margin-left: 10px;
+          a {
+            color: #fff;
+          }
+        }
+      }
+    }
+    .bottom {
+      .inner {
+        display: flex;
+        .column {
+          display: inline-block;
+          width: 20%;
+          height: 50px;
+          padding: 8px 10px 10px 10px;
+          border-right: 1px solid #eee;
+          text-align: center;
+          span {
+            display: inline-block;
+          }
+          .label {
+            width: 100%;
+            text-align: left;
+            font-size: 8px;
+            color: #999;
+          }
+          .value {
+            width: 100%;
+            text-align: left;
+            font-weight: bold;
+            font-size: 12px;
+            color: #333;
+          }
+        }
       }
     }
   }
